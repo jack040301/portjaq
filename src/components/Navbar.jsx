9255c6d306d66091da9ce8b5c74e1bf427312ec9
Jacqueline Porral <jackdaisuki04@gmail.com>
2022-09-16 12:32:10 +0800

    Navbar mobile fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,8 +19,8 @@ function Navbar() {
 
         </ul>
         <div onClick={handleNav} className='block md:hidden'>
-            {!nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
-        <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500': 'fixed left-[-100%]'}>
+            {!nav ? <AiOutlineMenu size={20}/> : < AiOutlineClose size={20}/>}
+        <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500': 'fixed left-[-100%]'}>
         <h1 className='w-full text-3xl font-bold  text-[#c0a1c3] m-4'>JAXK</h1>
             <ul className='uppercase p-4'>
             <li className='font-bold p-4 border-b border-b-slate-300'>Home</li>
